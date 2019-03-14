1a63441edf6c510dcb545d9107d4fdadc946b6ef
Waddlesworth <github@genj.io>
2019-03-14 16:33:47 +1100

    more GLTF stuff, correctly serializes indices

--- src/meshes.rs
+++ src/meshes.rs
@@ -3,15 +3,15 @@ use byteorder::{LittleEndian, WriteBytesExt};
 use std::path::Path;
 use crate::assets::{USkeletalMesh, ParserResult, ParserError, Package, FVector};
 mod gltf;
-use gltf::{GLTFItem, GLTFBuffer, GLTFBufferView};
+use gltf::{GLTFItem, GLTFBuffer, GLTFBufferView, GLTFAccessor, GLTFComponentType};
 
 
-pub struct GLTFContainer<'a> {
+pub struct GLTFContainer {
     pub buffer: Vec<u8>,
-    pub data: GLTFItem<'a>,
+    pub data: GLTFItem,
 }
 
-pub fn decode_mesh<'a>(package: Package, path: &str) -> ParserResult<GLTFContainer<'a>> {
+pub fn decode_mesh(package: Package, path: &str) -> ParserResult<GLTFContainer> {
     let filepath = Path::new(path);
     let filename = filepath.file_name().unwrap().to_str().unwrap().to_owned() + ".bin";
     let package_export = package.get_export_move(0)?;
@@ -22,17 +22,23 @@ pub fn decode_mesh<'a>(package: Package, path: &str) -> ParserResult<GLTFContain
     Err(ParserError::new(format!("Package not supported")))
 }
 
-fn decode_skeletal_mesh<'a>(mesh: USkeletalMesh, buffer_name: String) -> ParserResult<GLTFContainer<'a>> {
+fn decode_skeletal_mesh(mesh: USkeletalMesh, buffer_name: String) -> ParserResult<GLTFContainer> {
     let mut buffer: Vec<u8> = Vec::new();
     let mut mesh_data = GLTFItem::new();
+
 
     let position_verts = mesh.get_first_lod().get_position_buffer().get_verts();
     let position_size = write_verts_buffer(position_verts, &mut buffer)?;
     let position_buffer_view = GLTFBufferView::new(0, position_size);
-    mesh_data.add_buffer_view(position_buffer_view);
+    let buffer_view = mesh_data.add_buffer_view(position_buffer_view);
+
+    let mesh_accessor = GLTFAccessor::new(buffer_view, GLTFComponentType::Float, position_verts.len() as u32, "VEC3");
+    mesh_data.add_accessor(mesh_accessor);
 
     let buffer_desc = GLTFBuffer::new(buffer.len() as u32, buffer_name);
     mesh_data.add_buffer(buffer_desc);
+
+    
     
 
     Ok(GLTFContainer {
--- src/meshes/gltf.rs
+++ src/meshes/gltf.rs
@@ -1,15 +1,74 @@
-#[derive(Debug, Serialize)]
-pub struct GLTFItem<'a> {
+use std::cell::RefCell;
+use std::rc::Rc;
+use serde::ser::{Serialize, Serializer, SerializeStruct};
+
+pub trait Indexable {
+    fn get_index(&self) -> u32;
+    fn set_index(&mut self, index: u32);
+}
+
+#[derive(Debug)]
+pub struct RefItem<T> {
+    data: Rc<RefCell<T>>,
+}
+
+impl<T> RefItem<T> {
+    fn new(item: Rc<RefCell<T>>) -> Self {
+        Self {
+            data: item,
+        }
+    }
+}
+
+impl<T> Serialize for RefItem<T> where T: Indexable + Serialize {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        let item = self.data.borrow();
+        serializer.serialize_u32(item.get_index())
+    }
+}
+
+fn reindex_indexable<T>(items: &Vec<RefOwn<T>>) where T: Indexable {
+    let mut index = 0;
+    for item in items {
+        let mut node = item.data.borrow_mut();
+        node.set_index(index);
+        index += 1;
+    }
+}
+
+// Exists only so I can serialize an Rc<RefCell<T>>
+#[derive(Debug)]
+struct RefOwn<T> {
+    data: Rc<RefCell<T>>,
+}
+
+impl<T> RefOwn<T> {
+    fn new(item: Rc<RefCell<T>>) -> Self {
+        Self {
+            data: item,
+        }
+    }
+}
+
+impl<T> Serialize for RefOwn<T> where T: Serialize {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        let item = self.data.borrow();
+        item.serialize(serializer)
+    }
+}
+
+#[derive(Debug)]
+pub struct GLTFItem {
     asset: GLTFAsset,
     scene: u32,
     scenes: Vec<GLTFScene>,
-    nodes: Vec<GLTFNode<'a>>,
+    nodes: Vec<RefOwn<GLTFNode>>,
     buffers: Vec<GLTFBuffer>,
-    #[serde(rename="bufferViews")]
-    buffer_views: Vec<GLTFBufferView>,
+    buffer_views: Vec<RefOwn<GLTFBufferView>>,
+    accessors: Vec<RefOwn<GLTFAccessor>>,
 }
 
-impl<'a> GLTFItem<'a> {
+impl GLTFItem {
     pub fn new() -> Self {
         GLTFItem {
             asset: GLTFAsset {
@@ -24,28 +83,54 @@ impl<'a> GLTFItem<'a> {
             nodes: Vec::new(),
             buffers: Vec::new(),
             buffer_views: Vec::new(),
+            accessors: Vec::new(),
         }
     }
 
-    pub fn new_node<'b>(&'b mut self) -> &'a mut GLTFNode {
-        let index = self.nodes.len();
-        let node = GLTFNode {
+    pub fn new_node(&mut self) -> Rc<RefCell<GLTFNode>> {
+        let node = Rc::new(RefCell::new(GLTFNode {
             name: None,
             children: Vec::new(),
-        };
+            index: 0,
+        }));
 
-        self.nodes.push(node);
-        self.nodes.get_mut(index).unwrap()
+        self.nodes.push(RefOwn::new(node.clone()));
+        node
     }
 
-    pub fn add_buffer<'b>(&'b mut self, buffer: GLTFBuffer) {
+    pub fn add_buffer(&mut self, buffer: GLTFBuffer) {
         self.buffers.push(buffer);
     }
 
-    pub fn add_buffer_view(&'a mut self, buffer_view: GLTFBufferView) -> &'a GLTFBufferView {
-        let index = self.buffer_views.len();
-        self.buffer_views.push(buffer_view);
-        self.buffer_views.get(index).unwrap()
+    pub fn add_buffer_view(&mut self, buffer_view: GLTFBufferView) -> Rc<RefCell<GLTFBufferView>> {
+        let counted = Rc::new(RefCell::new(buffer_view));
+        self.buffer_views.push(RefOwn::new(counted.clone()));
+        counted
+    }
+
+    pub fn add_accessor(&mut self, accessor: GLTFAccessor) -> Rc<RefCell<GLTFAccessor>> {
+        let counted = Rc::new(RefCell::new(accessor));
+        self.accessors.push(RefOwn::new(counted.clone()));
+        counted
+    }
+}
+
+impl Serialize for GLTFItem {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        reindex_indexable(&self.nodes);
+        reindex_indexable(&self.buffer_views);
+        reindex_indexable(&self.accessors);
+
+        let mut state = serializer.serialize_struct("GLTFItem", 7)?;
+        state.serialize_field("asset", &self.asset)?;
+        state.serialize_field("scene", &self.scene)?;
+        state.serialize_field("scenes", &self.scenes)?;
+        state.serialize_field("nodes", &self.nodes)?;
+        state.serialize_field("buffers", &self.buffers)?;
+        state.serialize_field("bufferViews", &self.buffer_views)?;
+        state.serialize_field("accessors", &self.accessors)?;
+
+        state.end()
     }
 }
 
@@ -62,14 +147,26 @@ struct GLTFScene {
 }
 
 #[derive(Debug, Serialize)]
-pub struct GLTFNode<'a> {
+pub struct GLTFNode {
     name: Option<String>,
-    children: Vec<&'a GLTFNode<'a>>,
+    children: Vec<RefItem<GLTFNode>>,
+    #[serde(skip_serializing)]
+    index: u32,
 }
 
-impl<'a> GLTFNode<'a> {
-    pub fn add_child(&'a mut self, child_node: &'a GLTFNode) {
-        self.children.push(child_node);
+impl GLTFNode {
+    pub fn add_child(mut self, child_node: Rc<RefCell<GLTFNode>>) {
+        self.children.push(RefItem::new(child_node));
+    }
+}
+
+impl Indexable for GLTFNode {
+    fn get_index(&self) -> u32 {
+        self.index
+    }
+
+    fn set_index(&mut self, index: u32) {
+        self.index = index;
     }
 }
 
@@ -95,6 +192,8 @@ pub struct GLTFBufferView {
     #[serde(rename="byteOffset")]
     byte_offset: u32,
     buffer: u32,
+    #[serde(skip_serializing)]
+    index: u32,
 }
 
 impl GLTFBufferView {
@@ -102,6 +201,76 @@ impl GLTFBufferView {
         GLTFBufferView {
             byte_length, byte_offset,
             buffer: 0,
+            index: 0,
         }
+    }
+}
+
+impl Indexable for GLTFBufferView {
+    fn get_index(&self) -> u32 {
+        self.index
+    }
+
+    fn set_index(&mut self, index: u32) {
+        self.index = index;
+    }
+}
+
+#[derive(Debug)]
+pub enum GLTFComponentType {
+    Byte,
+    UnsignedByte,
+    Short,
+    UnsignedShort,
+    UnsignedInt,
+    Float,
+}
+
+impl Serialize for GLTFComponentType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        let val = match self {
+            GLTFComponentType::Byte => 5120,
+            GLTFComponentType::UnsignedByte => 5121,
+            GLTFComponentType::Short => 5122,
+            GLTFComponentType::UnsignedShort => 5123,
+            GLTFComponentType::UnsignedInt => 5125,
+            GLTFComponentType::Float => 5126,
+        };
+        serializer.serialize_u32(val)
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct GLTFAccessor {
+    #[serde(rename="bufferView")]
+    buffer_view: RefItem<GLTFBufferView>,
+    #[serde(rename="byteOffset")]
+    byte_offset: u32,
+    component_type: GLTFComponentType,
+    count: u32,
+    #[serde(rename="type")]
+    accessor_type: &'static str,
+    #[serde(skip_serializing)]
+    index: u32,
+}
+
+impl GLTFAccessor {
+    pub fn new(buffer_view: Rc<RefCell<GLTFBufferView>>, component_type: GLTFComponentType, count: u32, accessor_type: &'static str) -> Self {
+        Self {
+            buffer_view: RefItem::new(buffer_view), 
+            component_type, count, accessor_type,
+            index: 0,
+            byte_offset: 0,
+        }
+    }
+}
+
+impl Indexable for GLTFAccessor {
+    fn get_index(&self) -> u32 {
+        self.index
+    }
+
+    fn set_index(&mut self, index: u32) {
+        self.index = index;
     }
 }
